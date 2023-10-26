966c74dddd543c63519d18866c70effaf280c0f4
mike2339 <68823552+mike2339@users.noreply.github.com>
2023-10-26 09:07:48 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({ name: 'Temimills' })
+
+<iframe src="https://www.facebook.com/plugins/post.php?href=URL_TO_YOUR_FACEBOOK_POST&width=500" width="500" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
